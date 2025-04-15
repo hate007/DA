12a8463beb91537db62400a7ffb8b218c664886a
hate007 <151600506+hate007@users.noreply.github.com>
2025-04-15 21:50:14 +0530

    Update s6q1a.c

--- s6q1a.c
+++ s6q1a.c
@@ -1,55 +1,48 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <limits.h>
 #define V 5
-#define INF 99999  // A large number used as "infinity"
-
-int minKey(int key[], int mst[]) {
-    int min = INF, min_index;
-    for (int i = 0; i < V; i++) {
-        if (mst[i] == 0 && key[i] < min) {
-            min = key[i];
-            min_index = i;
-        }
-    }
+int minKey(int key[], bool mstSet[])
+{
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < V; v++)
+        if (mstSet[v] == false && key[v] < min)
+            min = key[v], min_index = v;
     return min_index;
 }
-
-void prim(int g[V][V]) {
-    int parent[V], key[V], mst[V];
-    for (int i = 0; i < V; i++) {
-        key[i] = INF;
-        mst[i] = 0;
-    }
-
+void printMST(int parent[], int graph[V][V])
+{
+    printf("Edge \tWeight\n");
+    for (int i = 1; i < V; i++)
+        printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
+}
+void primMST(int graph[V][V])
+{
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+    for (int i = 0; i < V; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
     key[0] = 0;
     parent[0] = -1;
-
-    for (int i = 0; i < V - 1; i++) {
-        int u = minKey(key, mst);
-        mst[u] = 1;
-
-        for (int v = 0; v < V; v++) {
-            if (g[u][v] && mst[v] == 0 && g[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = g[u][v];
-            }
-        }
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+        for (int v = 0; v < V; v++)
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+                parent[v] = u, key[v] = graph[u][v];
     }
-
-    for (int i = 1; i < V; i++) {
-        printf("%d - %d\t%d\n", parent[i], i, g[i][parent[i]]);
-    }
+    printMST(parent, graph);
 }
-
-int main() {
-    int g[V][V] = {
+int main()
+{
+    int graph[V][V] = {
         {0, 2, 0, 6, 0},
         {2, 0, 3, 8, 5},
         {0, 3, 0, 0, 7},
         {6, 8, 0, 0, 9},
-        {0, 5, 7, 9, 0}
-    };
-
-    prim(g);
+        {0, 5, 7, 9, 0}};
+    primMST(graph);
     return 0;
 }
